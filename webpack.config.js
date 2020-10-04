502a9498ac41b857d54d9eff8effd50ffa09451b
Mike Skalandunas <mskalandunas@gmail.com>
2020-10-03 22:01:36 -0700

    add  typescript support

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,21 @@ const path = require('path');
 module.exports = {
   entry: './src/index',
   mode: 'development',
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.ts$/,
+        use: ['babel-loader', 'ts-loader']
+      }
+    ]
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.bundle.js'
   },
-  plugins: [new HTMLWebpackPlugin()]
+  plugins: [new HTMLWebpackPlugin()],
+  resolve: {
+    extensions: ['.ts']
+  }
 };
