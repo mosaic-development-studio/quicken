8c3cd9ad2895f880ffe2a7cbb632b5b2ecba993c
Mike Skalandunas <mskalandunas@gmail.com>
2020-10-03 21:26:37 -0700

    add explicit entry and output

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+  entry: './src/index',
   mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js'
+  },
   plugins: [new HTMLWebpackPlugin()]
 };
